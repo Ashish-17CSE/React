c45681f432050f8c88e0083cec0891a3029e7dda
Ashish-17CSE <ashish.as28121998@gmail.com>
2023-10-08 01:31:15 +0530

    Conditional Rendering

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,10 +1,19 @@
 function ListGroup() {
-  const items = ["Patna", "Gaya", "Ara", "Arwal", "Aurangabad"];
+  let items = ["Patna", "Gaya", "Ara", "Arwal", "Aurangabad"];
+  items = [];
+
+  const getMessage = () => {
+    // return items.length === 0 ? <p>No item found</p> : null;
+
+    return items.length === 0 && <p>No item found</p>;
+  };
+
   return (
     // import { Fragment } from "react";
     // If add empty angle brackets "<>" we're telling react to use fragment
     <>
       <h1>List</h1>
+      {getMessage()}
       <ul className="list-group">
         {items.map((item) => (
           <li key={item}>{item}</li>
