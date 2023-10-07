1931f2200b73104c17f28df778fef5e056fb023a
Ashish-17CSE <ashish.as28121998@gmail.com>
2023-10-08 01:46:19 +0530

    Handling Events

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,22 +1,29 @@
+import { MouseEvent } from "react";
+
 function ListGroup() {
   let items = ["Patna", "Gaya", "Ara", "Arwal", "Aurangabad"];
-  items = [];
+  //   items = [];
 
   const getMessage = () => {
     // return items.length === 0 ? <p>No item found</p> : null;
-
     return items.length === 0 && <p>No item found</p>;
   };
+
+  // Event handler
+  const hendleClick = (event: MouseEvent) => console.log("Clicked");
 
   return (
     // import { Fragment } from "react";
     // If add empty angle brackets "<>" we're telling react to use fragment
     <>
       <h1>List</h1>
-      {getMessage()}
+      {/* {getMessage()} */}
+      {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map((item) => (
-          <li key={item}>{item}</li>
+        {items.map((item, index) => (
+          <li className="list-group-item" key={item} onClick={hendleClick}>
+            {item}
+          </li>
         ))}
       </ul>
     </>
