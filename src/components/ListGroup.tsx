d0252da459e85283a236292706fd0cd8fb816de2
Ashish-17CSE <ashish.as28121998@gmail.com>
2023-10-08 01:19:20 +0530

    Rendering Lists

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,14 +1,14 @@
 function ListGroup() {
+  const items = ["Patna", "Gaya", "Ara", "Arwal", "Aurangabad"];
   return (
     // import { Fragment } from "react";
     // If add empty angle brackets "<>" we're telling react to use fragment
     <>
+      <h1>List</h1>
       <ul className="list-group">
-        <li className="list-group-item">An item</li>
-        <li className="list-group-item">A second item</li>
-        <li className="list-group-item">A third item</li>
-        <li className="list-group-item">A fourth item</li>
-        <li className="list-group-item">And a fifth one</li>
+        {items.map((item) => (
+          <li key={item}>{item}</li>
+        ))}
       </ul>
     </>
   );
